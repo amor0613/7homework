2814176cda2effead712a26b4565101d19fd900f
amor0613 <498694005@qq.com>
2023-09-07 11:59:00 +0800

    自动推送

--- push.sh
+++ push.sh
@@ -2,5 +2,5 @@ git config --global user.name 'amor0613'
 git config --global user.email '498694005@qq.com'
 git config --global http.sslverify false
 git add .
-git commit -m $1
+git commit -m 自动推送
 git push
